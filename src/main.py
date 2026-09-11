07c6a47a0ffade649594d96b6821740564012923
Tjasa <53261472+TjasaZil@users.noreply.github.com>
2026-09-11 14:35:34 +0200

    Added some more changes

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from src.pipelines.customers import customers_pipeline
 from src.pipelines.products import products_pipeline
 from src.pipelines.orders import orders_pipeline
 
-customers_path="/data/customers.csv"
-orders_path="/data/orders.csv"
-products_path = "/data/products.csv"
+customers_path="data/customers.csv"
+orders_path="data/orders.csv"
+products_path = "data/products.csv"
 
 customers_pipeline(customers_path)
 products_pipeline(products_path)
